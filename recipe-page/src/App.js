c31d04ded6efe385e8e6181a3d6e3f299e128c1b
fwangaruu <156862619+fwangaruu@users.noreply.github.com>
2024-01-30 18:16:23 +0000

    added pictures and accordions

--- recipe-page/src/App.js
+++ recipe-page/src/App.js
@@ -8,6 +8,11 @@ import spongeboblogo from './images/spongebob-logo.png';
 import krabbypatty from './images/krabbypatty.jpg';
 import flan from './images/courage-flan.png';
 import logo from './images/logo.png';
+import ratlogo from '.images/ratlogo.png';
+import ratFood from '.images/ratFood.jpg';
+import frogfood from '.images/frogfood.jpg';
+import froglogo from '.images/froglogo/png';
+
 
 
 function App() {
@@ -41,6 +46,28 @@ const directions4 = ["In a large bowl, add ground beef  and seasonings and shape
 "Grill, covered, over medium heat or broil 4 in.", "Heat for 5-7 minutes on each side or until a thermometer reads 160° and juices run clear.", 
 "Serve on buns.",
 "Enjoy!"]
+
+const ingredients5 = ["3/4 cup lukewarm water, 3 1/2 teaspoons active dry yeast, 2 large eggs, 1/2 cup whole milk, 3 tablespoons unsalted butter, melted, 1/4 cup granulated sugar, plus 2 tablespoons, 4 ups bread flour, plus more for dusting, 1/2 teaspoon kosher salt, 8 cups canola oil, for frying, powdered sugar (for topping)"];
+const directions5 = ["1: Pour the water into a medium bowl or large liquid measuring cup and sprinkle the yeast over the top. Let stand for about 5 minutes, until foamy.",
+"2: Add the eggs, milk, melted butter, and sugar, and whisk to combine.",
+"3: In a large bowl, whisk together the flour and salt.",
+"4: Pour the wet ingredients into the dry ingredients, stirring with a flexible spatula to form a shaggy dough. Turn the dough out onto a clean surface and use your hands to knead until it comes together in a smooth ball, about 2 minutes.",
+"5: Grease a clean large bowl with canola oil. Transfer the dough to the greased bowl and cover tightly with plastic wrap. Let the dough rise in a warm place until doubled in size, about 1 hour.",
+"6: Punch the dough down with your fist, then turn it out onto a lightly floured surface.",
+"7: Roll the dough into a large rectangle, about ¼ inch (6 mm) thick. With a sharp knife, cut the dough into 2-inch (5 cm) squares, then transfer the squares to a baking sheet lined with parchment paper.",
+"8: Heat the canola oil in a large pot over medium-high heat until it reaches 350˚F (180˚C).",
+"9: Lower the dough squares, 3-4 at a time, into the hot oil. As soon as the beignets puff up, use tongs or a kitchen spider to gently flip them over. Fry until golden brown on both sides, flipping as needed, 2-3 minutes more. Transfer the beignets to a wire rack to drain.",
+"10: Dust the beignets with powdered sugar and serve warm. And enjoy!"];
+    
+const ingredients6 = ["1 (6 ounce) can tomato paste, 1/2 onion, chopped, 1/4 cup minced garlic, 3/4 cup water, 4 tablespoons olive oil, divided, salt and ground black pepper to taste, 1 small eggplant, trimmed and very thinly sliced, 1 zucchini, trimmed and very thinly sliced, 1 yellow squash, trimmed and very thinly sliced, 1 red bell pepper, cored and very thinly sliced, 1 yellow bell pepper, cored and very thinly sliced, 1 teaspoon fresh thyme leaves, 3 tablespoons mascarpone cheese"];
+const directions6 = ["Step 1: Preheat the oven to 375 degrees F (190 degrees C).",
+"Step 2: Spread tomato paste onto the bottom of a 10-inch square baking dish. Sprinkle with onion and garlic. Stir in water and 1 tablespoon olive oil until thoroughly combined. Season with salt and pepper.",
+"Step 3: Arrange alternating slices of eggplant, zucchini, yellow squash, red bell pepper, and yellow bell pepper, starting at the outer edge of the dish and working concentrically towards the center. Overlap slices a little to display colors.",
+"Step 4: Drizzle vegetables with remaining 3 tablespoons olive oil; season with salt and pepper. Sprinkle with thyme leaves. Cover vegetables with a piece of parchment paper cut to fit inside.",
+"Step 5: Bake in the preheated oven until vegetables are roasted and tender, about 45 minutes. Serve with dollops of mascarpone cheese."
+];
+
+
 
   return (
     <div className="App">
@@ -84,6 +111,23 @@ const directions4 = ["In a large bowl, add ground beef  and seasonings and shape
       
       />
 
+<Accordion 
+      header_Image={froglogo}
+      accordion_Image={frogfood}
+      title={"Princess and the Frog Beignets"}
+      description={"Indulge in the enchanting joy of Princess and the Frog Beignets—irresistibly fluffy, golden pillows of delight that bring a touch of Disney magic to your kitchen"}
+      ingredients={ingredients5}
+      directions={directions5}
+      />
+
+      <Accordion 
+      header_Image={ratlogo}
+      accordion_Image={ratFood}
+      title={"Remy's Ratatouille"}
+      description={"Take part in the favor packed, adventure with Remy's Ratatouille. Inspired by Disney's delightful rat chef, expertly layered vegetables dancing in perfect harmony, creating a symphony of taste that will transport you back to your childhood!"}
+      ingredients={ingredients6}
+      directions={directions6}
+      />
 
     </div>
   )
