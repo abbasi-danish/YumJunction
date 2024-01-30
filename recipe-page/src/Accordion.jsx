c0bf8e9fa771b88d7b0c49b3c64d5a0a8104f475
Yunus Kocaman <156374474+ykunus@users.noreply.github.com>
2024-01-30 09:05:18 +0000

    Finished the final version of the Accordion proponent. Now all you need to do is import the Accordion and fill the paramaters

--- recipe-page/src/Accordion.jsx
+++ recipe-page/src/Accordion.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import perfectSandwich from './images/perfect-sandwich.jpg';
-import adventureTime from './images/adventure-time.jpg';
 import logo from './images/logo.png';
 import './Accordion.css';
 
-export default function Accordion() {
+export default function Accordion({header, food, title, description, ingredients, directions}) {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <div className="App">
@@ -16,8 +14,8 @@ export default function Accordion() {
             </div>
             <div className="accordion">
                 <div onClick={() => setIsOpen(!isOpen)} className="accordion-header">
-                    <h2>Adventure Time: Jake's Perfect Sandwich</h2>
-                    <img className="accordion-image" src={adventureTime} alt='adventure-time'/>
+                    <h2>{title}</h2>
+                    <img className="accordion-image" src={header} alt='Cartoon Image'/>
                     <div className = "accordion-indicator">
                         {isOpen ? '-' : '+'}
                     </div>
@@ -26,28 +24,22 @@ export default function Accordion() {
                     <div className="accordion-body">
                         <div className="accordion-body-content">
                             <div className="accordion-body-content-left">
-                                <img className="perfect-sandwich" src={perfectSandwich} alt="perfect-sandwich" />
+                                <img className="perfect-sandwich" src={food} alt="Food Image" />
                             </div>
                             <div className="accordion-body-content-right">
-                                <h4>Description</h4>
-                                    <p>It's Adventure Time! This is a sandwich that Jake from Adventure Time would be proud of. It's loaded with bacon, lettuce, tomato, and a few other tasty ingredients.</p>
+                                <h3>Description</h3>
+                                <p>{description}</p>
                                 <h3>Ingredients</h3>
                                 <ul>
-                                    <li>1/2 cup of milk</li>
-                                    <li>1/2 cup of water</li>
-                                    <li>1/2 cup of sugar</li>
-                                    <li>1/2 cup of flour</li>
-                                    <li>1/2 cup of butter</li>
-                                    <li>1/2 cup of eggs</li>
-                                    <li>1/2 cup of vanilla</li>
-                                    <li>1/2 cup of salt</li>
-                                    <li>1/2 cup of baking soda</li>
+                                    {ingredients.map((ingredient, index) => (
+                                        <li key={index}>{ingredient}</li>
+                                    ))}
                                 </ul>
                                 <h3>Directions</h3>
                                 <ol>
-                                    <li>Preheat oven to 350 degrees F (175 degrees C). Grease and flour a 9x9 inch pan or line a muffin pan with paper liners.</li>
-                                    <li>In a medium bowl, cream together the sugar and butter. Beat in the eggs, one at a time, then stir in the vanilla. Combine flour and baking powder, add to the creamed mixture and mix well. Finally stir in the milk until batter is smooth. Pour or spoon batter into the prepared pan.</li>
-                                    <li>Bake for 30 to 40 minutes in the preheated oven. For cupcakes, bake 20 to 25 minutes. Cake is done when it springs back to the touch.</li>
+                                    {directions.map((direction, index) => (
+                                        <li key={index}>{direction}</li>
+                                    ))}
                                 </ol>
                             </div>
                         </div>
