c6a964c5fdd00704efdfafcc4a778e1e3f2d4915
abbasi-danish <danishabbasi@brandeis.edu>
2024-01-30 20:08:05 -0500

    fixed alt attributes

--- recipe-page/src/Accordion.jsx
+++ recipe-page/src/Accordion.jsx
@@ -8,7 +8,7 @@ export default function Accordion({header_Image, accordion_Image, title, descrip
             <div className="accordion">
                 <div onClick={() => setIsOpen(!isOpen)} className="accordion-header">
                     <h2>{title}</h2>
-                    <img className="header-image" src={header_Image} alt='cartoon character'/>
+                    <img className="header-image" src={header_Image} alt=''/>
                     <div className = "accordion-indicator">
                         {isOpen ? '-' : '+'}
                     </div>
@@ -18,7 +18,7 @@ export default function Accordion({header_Image, accordion_Image, title, descrip
                         <div className="accordion-body-content">
                             <div className="accordion-body-content-left">
 
-                                <img className="accordion_Image" src={accordion_Image} alt="delicious cartoon food" />
+                                <img className="accordion_Image" src={accordion_Image} alt="" />
                             </div>
                             <div className="accordion-body-content-right">
                                 <h3>Description</h3>
