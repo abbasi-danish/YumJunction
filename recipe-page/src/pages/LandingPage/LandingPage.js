ec6af55495772b1b63a5502ae9e22de73f36db99
abbasi-danish <danishabbasi@brandeis.edu>
2024-02-06 11:29:43 -0500

    commented out unused variables

--- recipe-page/src/pages/LandingPage/LandingPage.js
+++ recipe-page/src/pages/LandingPage/LandingPage.js
@@ -19,47 +19,47 @@ import './LandingPage.css';
 
 function LandingPage() {
 
-  const ingredients2 = ["285g all-purpose flour", "35g sugar", "1 ½ tsp baking powder", "1 ½ tsp baking soda", "Kosher salt", "2 ½ cups buttermilk", "2 eggs", "3 Tbsp melted butter", "Bacon", "Maple Syrup"];
-  const directions2 = [
-    "In a bowl, combine 285 grams of all-purpose flour, 35 grams of sugar, 1 ½ tsp each of baking powder and baking soda, and some kosher salt. Whisk to combine.",
-    "In the middle of the mixture create a little well and add 2 ½ cups of buttermilk along with 2 eggs and 3 Tbsp of melted butter. Whisk to combine. Be sure not to over whisk. Lumps are okay.",
-    "In a cast iron skillet, melt 1 Tbsp of butter over medium-high heat before adding your pancake batter.",
-    "Before flipping, add some strips of bacon and then flip.",
-    "Make 2-3 pancakes before topping with butter and maple syrup."
-];
+//   const ingredients2 = ["285g all-purpose flour", "35g sugar", "1 ½ tsp baking powder", "1 ½ tsp baking soda", "Kosher salt", "2 ½ cups buttermilk", "2 eggs", "3 Tbsp melted butter", "Bacon", "Maple Syrup"];
+//   const directions2 = [
+//     "In a bowl, combine 285 grams of all-purpose flour, 35 grams of sugar, 1 ½ tsp each of baking powder and baking soda, and some kosher salt. Whisk to combine.",
+//     "In the middle of the mixture create a little well and add 2 ½ cups of buttermilk along with 2 eggs and 3 Tbsp of melted butter. Whisk to combine. Be sure not to over whisk. Lumps are okay.",
+//     "In a cast iron skillet, melt 1 Tbsp of butter over medium-high heat before adding your pancake batter.",
+//     "Before flipping, add some strips of bacon and then flip.",
+//     "Make 2-3 pancakes before topping with butter and maple syrup."
+// ];
 
-const ingredients4 = ["2 cups ground beef", "1/4 cup breadcrumbs", "1/4 cup chopped onions", "1/4 cup chopped pickles", "1/4 cup ketchup", "1/4 cup mustard", "1/4 teaspoon salt", "1/4 teaspoon pepper", "4 hamburger buns"]
-const directions4 = ["In a large bowl, add ground beef  and seasonings and shape into four patties", 
-"Grill, covered, over medium heat or broil 4 in.", "Heat for 5-7 minutes on each side or until a thermometer reads 160° and juices run clear.", 
-"Serve on buns.",
-"Enjoy!"];
+// const ingredients4 = ["2 cups ground beef", "1/4 cup breadcrumbs", "1/4 cup chopped onions", "1/4 cup chopped pickles", "1/4 cup ketchup", "1/4 cup mustard", "1/4 teaspoon salt", "1/4 teaspoon pepper", "4 hamburger buns"]
+// const directions4 = ["In a large bowl, add ground beef  and seasonings and shape into four patties", 
+// "Grill, covered, over medium heat or broil 4 in.", "Heat for 5-7 minutes on each side or until a thermometer reads 160° and juices run clear.", 
+// "Serve on buns.",
+// "Enjoy!"];
 
-const ingredients7 =["4 ripe bananas, Yellow food coloring"];
-const directions7 = [
-  "Peel and thinly slice the bananas. Freeze the banana slices for at least two hours. Place the frozen banana slices in a food processor.",
-  "Blend, occasionally scraping down the sides of the bowl, until the bananas are smooth and creamy. Add a few drops of yellow food coloring and blend until the color is evenly distributed.",
-  "Serve immediately or freeze for later. Enjoy!"
-];
+// const ingredients7 =["4 ripe bananas, Yellow food coloring"];
+// const directions7 = [
+//   "Peel and thinly slice the bananas. Freeze the banana slices for at least two hours. Place the frozen banana slices in a food processor.",
+//   "Blend, occasionally scraping down the sides of the bowl, until the bananas are smooth and creamy. Add a few drops of yellow food coloring and blend until the color is evenly distributed.",
+//   "Serve immediately or freeze for later. Enjoy!"
+// ];
 
-const ingredients5 = ["3/4 cup lukewarm water, 3 1/2 teaspoons active dry yeast, 2 large eggs, 1/2 cup whole milk, 3 tablespoons unsalted butter, melted, 1/4 cup granulated sugar, plus 2 tablespoons, 4 ups bread flour, plus more for dusting, 1/2 teaspoon kosher salt, 8 cups canola oil, for frying, powdered sugar (for topping)"];
-const directions5 = ["Pour the water into a medium bowl or large liquid measuring cup and sprinkle the yeast over the top. Let stand for about 5 minutes, until foamy.",
-"Add the eggs, milk, melted butter, and sugar, and whisk to combine.",
-"In a large bowl, whisk together the flour and salt.",
-"Pour the wet ingredients into the dry ingredients, stirring with a flexible spatula to form a shaggy dough. Turn the dough out onto a clean surface and use your hands to knead until it comes together in a smooth ball, about 2 minutes.",
-"Grease a clean large bowl with canola oil. Transfer the dough to the greased bowl and cover tightly with plastic wrap. Let the dough rise in a warm place until doubled in size, about 1 hour.",
-"Punch the dough down with your fist, then turn it out onto a lightly floured surface.",
-"Roll the dough into a large rectangle, about ¼ inch (6 mm) thick. With a sharp knife, cut the dough into 2-inch (5 cm) squares, then transfer the squares to a baking sheet lined with parchment paper.",
-"Heat the canola oil in a large pot over medium-high heat until it reaches 350˚F (180˚C).",
-"Lower the dough squares, 3-4 at a time, into the hot oil. As soon as the beignets puff up, use tongs or a kitchen spider to gently flip them over. Fry until golden brown on both sides, flipping as needed, 2-3 minutes more. Transfer the beignets to a wire rack to drain.",
-"Dust the beignets with powdered sugar and serve warm. And enjoy!"];
+// const ingredients5 = ["3/4 cup lukewarm water, 3 1/2 teaspoons active dry yeast, 2 large eggs, 1/2 cup whole milk, 3 tablespoons unsalted butter, melted, 1/4 cup granulated sugar, plus 2 tablespoons, 4 ups bread flour, plus more for dusting, 1/2 teaspoon kosher salt, 8 cups canola oil, for frying, powdered sugar (for topping)"];
+// const directions5 = ["Pour the water into a medium bowl or large liquid measuring cup and sprinkle the yeast over the top. Let stand for about 5 minutes, until foamy.",
+// "Add the eggs, milk, melted butter, and sugar, and whisk to combine.",
+// "In a large bowl, whisk together the flour and salt.",
+// "Pour the wet ingredients into the dry ingredients, stirring with a flexible spatula to form a shaggy dough. Turn the dough out onto a clean surface and use your hands to knead until it comes together in a smooth ball, about 2 minutes.",
+// "Grease a clean large bowl with canola oil. Transfer the dough to the greased bowl and cover tightly with plastic wrap. Let the dough rise in a warm place until doubled in size, about 1 hour.",
+// "Punch the dough down with your fist, then turn it out onto a lightly floured surface.",
+// "Roll the dough into a large rectangle, about ¼ inch (6 mm) thick. With a sharp knife, cut the dough into 2-inch (5 cm) squares, then transfer the squares to a baking sheet lined with parchment paper.",
+// "Heat the canola oil in a large pot over medium-high heat until it reaches 350˚F (180˚C).",
+// "Lower the dough squares, 3-4 at a time, into the hot oil. As soon as the beignets puff up, use tongs or a kitchen spider to gently flip them over. Fry until golden brown on both sides, flipping as needed, 2-3 minutes more. Transfer the beignets to a wire rack to drain.",
+// "Dust the beignets with powdered sugar and serve warm. And enjoy!"];
     
-const ingredients6 = ["1 (6 ounce) can tomato paste, 1/2 onion, chopped, 1/4 cup minced garlic, 3/4 cup water, 4 tablespoons olive oil, divided, salt and ground black pepper to taste, 1 small eggplant, trimmed and very thinly sliced, 1 zucchini, trimmed and very thinly sliced, 1 yellow squash, trimmed and very thinly sliced, 1 red bell pepper, cored and very thinly sliced, 1 yellow bell pepper, cored and very thinly sliced, 1 teaspoon fresh thyme leaves, 3 tablespoons mascarpone cheese"];
-const directions6 = ["Preheat the oven to 375 degrees F (190 degrees C).",
-"Spread tomato paste onto the bottom of a 10-inch square baking dish. Sprinkle with onion and garlic. Stir in water and 1 tablespoon olive oil until thoroughly combined. Season with salt and pepper.",
-"Arrange alternating slices of eggplant, zucchini, yellow squash, red bell pepper, and yellow bell pepper, starting at the outer edge of the dish and working concentrically towards the center. Overlap slices a little to display colors.",
-"Drizzle vegetables with remaining 3 tablespoons olive oil; season with salt and pepper. Sprinkle with thyme leaves. Cover vegetables with a piece of parchment paper cut to fit inside.",
-"Bake in the preheated oven until vegetables are roasted and tender, about 45 minutes. Serve with dollops of mascarpone cheese."
-];
+// const ingredients6 = ["1 (6 ounce) can tomato paste, 1/2 onion, chopped, 1/4 cup minced garlic, 3/4 cup water, 4 tablespoons olive oil, divided, salt and ground black pepper to taste, 1 small eggplant, trimmed and very thinly sliced, 1 zucchini, trimmed and very thinly sliced, 1 yellow squash, trimmed and very thinly sliced, 1 red bell pepper, cored and very thinly sliced, 1 yellow bell pepper, cored and very thinly sliced, 1 teaspoon fresh thyme leaves, 3 tablespoons mascarpone cheese"];
+// const directions6 = ["Preheat the oven to 375 degrees F (190 degrees C).",
+// "Spread tomato paste onto the bottom of a 10-inch square baking dish. Sprinkle with onion and garlic. Stir in water and 1 tablespoon olive oil until thoroughly combined. Season with salt and pepper.",
+// "Arrange alternating slices of eggplant, zucchini, yellow squash, red bell pepper, and yellow bell pepper, starting at the outer edge of the dish and working concentrically towards the center. Overlap slices a little to display colors.",
+// "Drizzle vegetables with remaining 3 tablespoons olive oil; season with salt and pepper. Sprinkle with thyme leaves. Cover vegetables with a piece of parchment paper cut to fit inside.",
+// "Bake in the preheated oven until vegetables are roasted and tender, about 45 minutes. Serve with dollops of mascarpone cheese."
+// ];
 
 
 
