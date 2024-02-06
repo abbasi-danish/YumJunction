4d30e1be69ed898c7d5b7ee5be467d7b9795b85f
Yunus Kocaman <156374474+ykunus@users.noreply.github.com>
2024-02-06 16:41:51 +0000

    made it where all pictures are the same size, added three more recipes

--- recipe-page/src/App.js
+++ recipe-page/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Recipe1 from './pages/Recipe1/Recipe1';
+import Recipe5 from './pages/Recipe5/Recipe5';
+import Recipe3 from './pages/Recipe3/Recipe3';
+import Recipe4 from './pages/Recipe4/Recipe4';
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/recipe1" element={<Recipe1 />} />
+                <Route path="/recipe5" element={<Recipe5 />} />
+                <Route path="/recipe3" element={<Recipe3 />} />
+                <Route path="/recipe4" element={<Recipe4 />} />
             </Routes>
         </Router>
     );
