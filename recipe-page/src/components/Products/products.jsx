562aa3f2cdac44ce4ed6d05a322d4a362f7272f3
joseph-sosa <156862452+joseph-sosa@users.noreply.github.com>
2024-02-13 17:47:53 +0000

    added a product component

---
+++ recipe-page/src/components/Products/products.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Products({ name,}) {
+    return (
+        <div className="Products">
+            <h2>{name}:</h2>
+        </div>
+    );
+}
+
