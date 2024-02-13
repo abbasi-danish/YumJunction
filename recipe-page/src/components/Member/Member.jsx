80c7ff4ea78c5c95821cb2f56e5de26aabe2152c
Yunus Kocaman <156374474+ykunus@users.noreply.github.com>
2024-02-13 04:14:37 +0000

    added a new style for the team page. Not finished yet

--- recipe-page/src/components/Member/Member.jsx
+++ recipe-page/src/components/Member/Member.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 export default function Member({ memberImage, name, description}) {
     return (
-        <div className="Member">
-            <div style={{ float: "left" }}>
-                <img className = "memberImage" src={memberImage} alt="Member" />
-            </div>
-            <h2>{name}:</h2>
-            <p>{description}</p>
+        <div className="member_wrapper">
+            {/* <div className="member_details"> */}
+                {/* <div className="member_name"> */}
+                    <img className="member_img" src={memberImage} alt="Member"/>
+                    <div className='member_detatis'>
+                        <h4>{name}:</h4>
+                        <p>{description}</p>
+                    </div>
+                {/* </div> */}
+            {/* </div> */}
         </div>
     );
 }
--- recipe-page/src/pages/TeamLanding/TeamLanding.js
+++ recipe-page/src/pages/TeamLanding/TeamLanding.js
@@ -4,34 +4,36 @@ import yunus from '../../images/yunus.jpg';
 import pakistan from '../../images/Pakistan.png';
 import sosa from '../../images/sosa.jpg';
 
-import './TeamLanding.css'; // Import the CSS file for styling
+import './TeamLanding.css'; 
 function TeamLanding() {
     return (
         <div className="TeamLanding">
             <div className="header" style={{ borderBottom: "3px solid black", width: "100%" }}>
                 <h1>Meet The Team</h1>
             </div>
-            <div className="members-container"> 
-                <Member
-                    name="Yunus Kocaman"
-                    description="Yunus Kocaman is a sophomore at Brandeis University. He is majoring in Computer Science and is intrested in full stack developing."
-                    memberImage={yunus}
-                />
-                <Member
-                    name="Danish Abbasi"
-                    description="Patrick Star is SpongeBob's best friend. He is known for his love of ice cream and his lack of intelligence."
-                    memberImage={pakistan}
-                />
-                <Member
-                    name ="Joseph Sosa"
-                    description="Joseph Sosa is SpongeBob's neighbor. He is known for his love of music and his dislike of SpongeBob."
-                    memberImage={sosa}
-                />
-                <Member
-                    name ="Fitz Gerald"
-                    description="Mr. Krabs is the owner of the Krusty Krab. He is known for his love of money and his greed."   
-                    memberImage={pakistan}
-                />
+            <div className="member_wrapper">
+                <div className="member_name">
+                    <Member
+                        name="Yunus Kocaman"
+                        description="Yunus Kocaman is a sophomore at Brandeis University. He is majoring in Computer Science and is intrested in full stack developing."
+                        memberImage={yunus}
+                    />
+                    <Member
+                        name="Danish Abbasi"
+                        description="Patrick Star is SpongeBob's best friend. He is known for his love of ice cream and his lack of intelligence."
+                        memberImage={pakistan}
+                    />
+                    <Member
+                        name ="Joseph Sosa"
+                        description="Joseph Sosa is SpongeBob's neighbor. He is known for his love of music and his dislike of SpongeBob."
+                        memberImage={sosa}
+                    />
+                    <Member
+                        name ="Fitz Gerald"
+                        description="Mr. Krabs is the owner of the Krusty Krab. He is known for his love of money and his greed."   
+                        memberImage={pakistan}
+                    />
+                </div>
             </div>
         </div>
     );
