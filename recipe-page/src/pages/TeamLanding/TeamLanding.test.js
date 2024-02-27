2f6954fed9e33326156d139ec152b908c162ca35
Yunus Kocaman <156374474+ykunus@users.noreply.github.com>
2024-02-27 19:40:51 +0000

    Modified the team page and changed tests

--- recipe-page/src/pages/TeamLanding/TeamLanding.test.js
+++ recipe-page/src/pages/TeamLanding/TeamLanding.test.js
@@ -10,16 +10,16 @@ test('renders team landing page', () => {
 
 
     // Assert that the team members are rendered
-    // const member1Element = screen.getByText(/Yunus Kocaman is a sophomore at Brandeis University. He is majoring in Computer Science and is interested in full stack development."/i);
-    // expect(member1Element).toBeInTheDocument();
+    const member1Element = screen.getByText(/Software Engineer/i);
+    expect(member1Element).toBeInTheDocument();
 
-    const member2Element = screen.getByText(/Danish Abbasi is SpongeBob's best friend. He is known for his love of ice cream and his lack of intelligence./i);
+    const member2Element = screen.getByText(/Software Engineer/i);
    expect(member2Element).toBeInTheDocument();
 
-    const member3Element = screen.getByText(/Joseph Sosa is SpongeBob's neighbor. He is known for his love of music and his dislike of SpongeBob./i);
+    const member3Element = screen.getByText(/Software Engineer/i);
     expect(member3Element).toBeInTheDocument();
 
-    const member4Element = screen.getByText(/Fitz Gerald is the owner of the Krusty Krab. He is known for his love of money and his greed./i);
+    const member4Element = screen.getByText(/Software Engineer/i);
     expect(member4Element).toBeInTheDocument();
     
     // Assert that the images are rendered
