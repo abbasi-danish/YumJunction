a8f29b532a9634c4b7e91497a087a35d8bbb02f7
joseph-sosa <156862452+joseph-sosa@users.noreply.github.com>
2024-03-19 01:29:18 +0000

    added cooking mode button and funcationality

--- recipe-page/src/pages/Recipe1/Recipe1.js
+++ recipe-page/src/pages/Recipe1/Recipe1.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import perfectSandwich from '../../images/perfect-sandwich.jpg';
 import NewPage from '../../components/NewPage/NewPage';
 import Carousel from '../../components/Carousel/Carousel';
@@ -13,28 +13,45 @@ function Recipe1() {
         "Assemble the sandwich - halve the loaf, and torch both sides of the interior with a culinary torch (only for accuracy - recommend toasting bread in the oven or throwing under the broiler).  Spread with cream cheese, and shingle sliced pickles on one side of the sandwich, while sprinkling dill on the other side of the sandwich.  On the pickle side of the sandwich, shingle the hard-boiled egg slices, followed by the Cornish game hen, followed by cucumber, then sweet yellow onion, Roma tomatoes, steak, tears, and bacon.  Coat the other side of the bread with lobster aioli, top the sandwich, slice, and serve."
     ];
 
+    const [cookingMode, setCookingMode] = useState(false);
 
+    const toggleCookingMode = () => {
+        setCookingMode(!cookingMode);
+    };
+
+    const exitCookingMode = () => {
+        setCookingMode(false);
+    };
 
     return (
         <div>
-            <div className='cookingMode'> 
-                <Carousel
-                    slides={[
-                        { type: 'text', content: 'Vacuum seal ribeye with thyme, rosemary, salt, and pepper, and place in a sous vide bath set at 135°F for 2 hours, Preheat oven to 350°F.  Steam lobster tail for for 8 minutes, remove meat and reserve for other applications.  Break up the shell into pieces and place in a high-powered blender along with vegetable oil, and blend for 30-60 seconds:' },
-                        { type: 'text', content: 'or until lobster is finely ground.  Simmer mixture for 15 minutes, and strain through a fine mesh sieve.  Set aside to cool completely. In an immersion blender cup, combine lemon and egg yolks.  Blend briefly to combine, and with immersion blender running, slowly pour lobster oil down the side of the cup, until a thick aioli forms.  Refrigerate until ready to use.' },
-                    ]}
-                    /* Each slide will contain a new step*/
-                    title='The Perfect Sandwich'
-                />
+            <div className='cookingMode'>
+                {cookingMode ? (
+                    <div>
+                        <Carousel
+                            slides={directions.map((direction, index) => ({
+                                type: 'text',
+                                content: direction,
+                                id: index,
+                            }))}
+                            title='Cooking Mode'
+                        />
+                        <button onClick={exitCookingMode}>Exit Cooking Mode</button>
+                    </div>
+                ) : (
+                    <button onClick={toggleCookingMode}>Cooking Mode</button>
+                )}
             </div>
-            <div className='recipe'>
-                <NewPage
-                    title='The Perfect Sandwich'
-                    pageImage={perfectSandwich}
-                    ingredients={ingredients}
-                    directions={directions}
-                />
-            </div>
+            {!cookingMode && (
+                <div className='recipe'>
+                    <NewPage
+                        title='The Perfect Sandwich'
+                        pageImage={perfectSandwich}
+                        ingredients={ingredients}
+                        directions={directions}
+                    />
+                </div>
+            )}
         </div>
     );
 }
