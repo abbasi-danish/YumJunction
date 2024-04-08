d2c6f72c1d59ec2226b38ef6df03c2ce6fde70ac
fwangaruu <156862619+fwangaruu@users.noreply.github.com>
2024-04-08 02:25:00 +0000

    added more to the search bar

--- recipe-page/src/components/SearchAPI/SearchBar.jsx
+++ recipe-page/src/components/SearchAPI/SearchBar.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./SearchBar.css";
 
 
 function SearchBar() {
+    useEffect(() => {
+        fetch ('https://api.spoonacular.com/recipes/complexSearch?apiKey=4e0b1f3e2c7e4b5a8f7c3f6f9c8b3b5a&query=burger')
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .then(err => console.log(err))
+    },[]);
     return (
         <div className="search">
             <input type ="text " placeholder="Search here..."/>
