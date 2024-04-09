963cd7d28ca3d542c40ad3e76b091c129ab76794
abbasi-danish <danishabbasi@brandeis.edu>
2024-04-09 17:51:59 -0400

    set up skeleton for cosmosdb file

--- recipe-page/backend/react-node-app/server/cosmosDB.js
+++ recipe-page/backend/react-node-app/server/cosmosDB.js
@@ -1,4 +1,5 @@
 const { CosmosClient } = require("@azure/cosmos");
+const COSMOS_KEY = process.env.connection_string;
 
-const cosmosClient = new CosmosClient({ connectionString: "<your-connection-string>" });
-const container = cosmosClient.database("<your-database-name>").container("<your-container-name>");
+const cosmosClient = new CosmosClient({COSMOS_KEY});
+const container = cosmosClient.database("recipe-db").container("recipes");
